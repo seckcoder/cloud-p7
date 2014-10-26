8180fdc648cf91499c6624279f8ab36137ae9af1
seckcoder <seckcoder@gmail.com>
2014-10-26 13:04:05 -0700

    update

--- start_send_mysql_tps.sh
+++ start_send_mysql_tps.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-curl https://bitbucket.org/seckcoder/cloud-storage/raw/72e5dd4d97842c9fd2db40654bd585406db4ade9/send_mysql_tps.py -o ~/bin/send_msql_tps.py
-
-(
-python ~/bin/send_mysql_tps.py &>~/log/mysql_tps.log
-)
-
+curl https://raw.githubusercontent.com/seckcoder/cloud-p7/master/send_mysql_tps.py -o ~/bin/send_mysql_tps.py
+python ~/bin/send_mysql_tps.py &>~/log/mysql_tps.log &
